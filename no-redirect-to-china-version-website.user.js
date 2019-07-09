d14acf4f79397924587a9f6af200016889e54680
Newname <new@email>
2019-07-09 22:20:58 +0800

    Update no-redirect-to-china-version-website.user.js

--- no-redirect-to-china-version-website.user.js
+++ no-redirect-to-china-version-website.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Do not redirect to China Version Website
 // @namespace    ATGT
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        *://*.leetcode-cn.com/*
@@ -19,12 +19,14 @@
 		console.log('new location ', location.host);
 	} else if (/leetcode.com$/.test(location.host)) {
 		setTimeout(() => {
+			// close banner on main page
 			let btn = document.querySelector('#region_switcher .close-btn');
 			if (btn)
 				btn.click();
 
 		}, 500);
 		setTimeout(() => {
+			// close banner on problems page
 			let btn = document.querySelector('#CNbanner .cn_close_btn');
 			if (btn)
 				btn.click();
