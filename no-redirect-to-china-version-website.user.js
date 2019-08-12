d9cdaafab6d08b0c06533097768d10c83e93456c
Newname <new@email>
2019-08-12 10:00:31 +0800

    Update no redir to cn sites

--- no-redirect-to-china-version-website.user.js
+++ no-redirect-to-china-version-website.user.js
@@ -1,37 +1,42 @@
 // ==UserScript==
 // @name         Do not redirect to China Version Website
 // @namespace    ATGT
-// @version      0.3
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
 // @match        *://*.leetcode-cn.com/*
 // @match        *://*.leetcode.com/*
 // @grant        none
-// @run-at		 document-end
+// @run-at		 document-start
 // ==/UserScript==
 
 (function() {
 	console.log(`=== do not direct to china version web size ${location.host} ===`);
 	'use strict';
-	if (/leetcode-cn.com$/.test(location.host)) {
+	if (/leetcode-cn.com$/.test(location.host) || /utm_\w+/.test(location.search)) {
 		console.log('redirect to leetcode.com');
-		location.host = location.host.replace(/leetcode-cn.com$/, 'leetcode.com');
-		console.log('new location ', location.host);
-	} else if (/leetcode.com$/.test(location.host)) {
-		setTimeout(() => {
-			// close banner on main page
-			let btn = document.querySelector('#region_switcher .close-btn');
-			if (btn)
-				btn.click();
+		let enUrl = new URL(location.href);
+		enUrl.host = enUrl.host.replace(/leetcode-cn.com$/, 'leetcode.com');
+		enUrl.search = enUrl.search.replace(/utm_[^&]+&?/, '');
+		enUrl.search = enUrl.search.replace(/\?$/, '');
+		location.href = enUrl.href;
+	}
+	if (/leetcode.com$/.test(location.host)) {
+		window.addEventListener('load',() => {
+			setTimeout(() => {
+				// close banner on main page
+				let btn = document.querySelector('#region_switcher .close-btn');
+				if (btn)
+					btn.click();
 
-		}, 500);
-		setTimeout(() => {
-			// close banner on problems page
-			let btn = document.querySelector('#CNbanner .cn_close_btn');
-			if (btn)
-				btn.click();
-		}, 1500);
-
+			}, 500);
+			setTimeout(() => {
+				// close banner on problems page
+				let btn = document.querySelector('#CNbanner .cn_close_btn');
+				if (btn)
+					btn.click();
+			}, 1500);
+		});
 	}
 	console.log(`=== /do not direct to china version web size ${location.host} ===`);
 })();
