5d85fc2cb2fbd47786a4a42e81e4638ed18ca2b0
Newname <new@email>
2023-03-18 14:55:23 +0800

    more generic site keymap

--- video-player-keyboard-shortcut.user.js
+++ video-player-keyboard-shortcut.user.js
@@ -4,6 +4,7 @@
 // @description  video player keyboard shortcut
 // @version  1
 // @match    https://v.youku.com/v_show/*
+// @match    https://www.ixigua.com/*
 // @run-at   document-start
 // ==/UserScript==
 
@@ -13,17 +14,41 @@ let warn = console.warn;
 let error = console.error;
 
 warn(`=== video-player-keyboard-shortcut on '${location.href}' ===`);
-function injectYoukuShortcut()
+
+let site_keymaps = [
+	{
+		url: /^https:\/\/v\.youku\.com\/v_show\//,
+		keymap: {
+			'w': 'icon.kui-webfullscreen-icon-0',
+			'f': 'icon.kui-fullscreen-icon-0',
+			'n': 'icon.kui-next-icon-0',
+			'p': 'button#pictureInPictureToggle'
+		},
+	}, {
+		url: /^https:\/\/www\.ixigua\.com\//,
+		keymap: {
+			'f': 'div[aria-label$="全屏"]',
+		}
+	}
+]
+
+function reg_event_handler(keymap)
 {
 	document.addEventListener('keyup', function (event) {
-		if (event.key === 'f' || event.key === 'w') {
-			warn('event ', event)
-			let element = document.querySelector(event.key == 'w' && 'icon.kui-webfullscreen-icon-0' || 'icon.kui-fullscreen-icon-0');
+		// warn('event ', event)
+		let sel = keymap[event.key];
+		warn('sel ', sel);
+		if (sel) {
+			let element = document.querySelector(sel);
 			element && element.click();
 		}
 	});
 }
 
-/^https:\/\/v\.youku\.com\/v_show\//.test(location.href) && injectYoukuShortcut();
+for (let skm of site_keymaps) {
+	if (skm.url.test(location.href)) {
+		reg_event_handler(skm.keymap);
+	}
+}
 
 warn(`=== video-player-keyboard-shortcut on '${location.href}' ===`);
